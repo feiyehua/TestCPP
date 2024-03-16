a7f403db8219a35ed1660f0f4e6bb99213480358
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2024-03-16 21:39:03 +0800

    Initial Commit

---
+++ TestCPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestCPP
+//
+//  Created by 熊泗六 on 2024/3/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestCPP/TestCPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCPPApp.swift
+//  TestCPP
+//
+//  Created by 熊泗六 on 2024/3/16.
+//
+
+import SwiftUI
+
+@main
+struct TestCPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
